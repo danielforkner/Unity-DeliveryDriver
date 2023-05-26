6de228c87e26d36701dc6b5a5727cabd4d1ee8c8
danielforkner <daniel.forkner@gmail.com>
2023-05-26 17:33:08 -0400

    initial commit

---
+++ Assets/Driver.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 0.1f;
+    [SerializeField] float moveSpeed = 0.01f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * -1;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        transform.Rotate(0, 0, steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+}
