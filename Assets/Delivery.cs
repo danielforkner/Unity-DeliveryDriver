06a2509a438073c27c7ba620a8ef6e9bb74d21bd
danielforkner <daniel.forkner@gmail.com>
2023-05-28 11:56:49 -0400

    dynamic car speeds; correct steering angles

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -6,7 +6,13 @@ public class Delivery : MonoBehaviour
 {
     bool hasPackage = false;
     [SerializeField] float destroyDelay = 0.5f;
-
+    [SerializeField] Color32 hasPackageColor = new Color32(239, 251, 90, 255);
+    [SerializeField] Color32 noPackageColor = new Color32(255, 255, 255, 255);
+    SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     private void OnCollisionEnter2D(Collision2D collision) 
     {
         Debug.Log("Ouch!");
@@ -18,11 +24,14 @@ public class Delivery : MonoBehaviour
             Debug.Log("Package picked up!");
             hasPackage = true;
             Destroy(collider.gameObject, destroyDelay);
+            spriteRenderer.color = hasPackageColor;
         }
         if (collider.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package Delivered!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+
         }
     }
 }
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -6,15 +6,35 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 250f;
     [SerializeField] float moveSpeed = 20f;
-    void Start()
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
+    float currentMoveSpeed = 20f;
+
+    private void OnTriggerEnter2D(Collider2D collider) 
     {
-        
+        if (collider.tag == "Boost")
+        {
+            currentMoveSpeed = boostSpeed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        currentMoveSpeed = slowSpeed;
     }
 
     void Update()
     {
+        float moveAmount = Input.GetAxis("Vertical") * currentMoveSpeed * Time.deltaTime;
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        if (moveAmount == 0) 
+        {
+            steerAmount = 0;
+        }
+        if (moveAmount < 0)
+        {
+            steerAmount *= -1;
+        }
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
